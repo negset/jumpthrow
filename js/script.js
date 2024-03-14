28219fe80d1b3d912d952a712b5686caaa1837cb
negset <15167516+negset@users.noreply.github.com>
2024-03-15 03:31:02 +0900

    update style

--- js/script.js
+++ js/script.js
@@ -5,41 +5,43 @@ let attempts = 0;
 let successes = 0;
 let wrongInputs = 0;
 let deltaSum = 0;
+let aceCounter = 0;
 
-let jumpAssign = {type: "keydown", code: "Space"};
-let throwAssign = {type: "mousedown", button: 0};
+let jumpAssign = { type: "keydown", code: "Space", name: "Space" };
+let throwAssign = { type: "mousedown", button: 0, name: "Mousebutton 0" };
 
 function insertResult(isSuccess, isMultipleInput, delta) {
-  const result = document.createElement("p");
+  const result = document.createElement("div");
   if (isMultipleInput) {
     result.innerHTML = `Multiple ${timeJump > 0 ? "jumps" : "throws"} detected`;
-    result.style.color = "gray";
   } else {
     result.innerHTML = `${isSuccess ? "Success" : "Failure"} (${delta} ms)`;
-    result.style.color = isSuccess ? "green" : "red";
+    result.classList.add(isSuccess ? "success" : "failure");
+    aceCounter = 0;
   }
   const parent = document.getElementById("results");
   parent.appendChild(result);
-  result.scrollIntoView({behavior: "smooth"});
+  result.scrollIntoView({ behavior: "smooth" });
 }
 
 function updateAnalytics() {
   const anlAttempts = document.getElementById("anl-attempts");
+  const anlSuccesses = document.getElementById("anl-successes");
   const anlSuccessRate = document.getElementById("anl-success-rate");
   const anlWrongInputFirst = document.getElementById("anl-wrong-input-first");
   const anlThrowTooLate = document.getElementById("anl-throw-too-late");
   const anlAverageDelta = document.getElementById("anl-average-delta");
   anlAttempts.innerHTML = attempts;
-  anlSuccessRate.innerHTML = 100 * successes / attempts;
-  anlWrongInputFirst.innerHTML = 100 * wrongInputs / attempts;
-  anlThrowTooLate.innerHTML = 100 * (attempts - successes - wrongInputs) / attempts;
+  anlSuccesses.innerHTML = successes;
+  anlSuccessRate.innerHTML = (100 * successes) / attempts;
+  anlWrongInputFirst.innerHTML = (100 * wrongInputs) / attempts;
+  anlThrowTooLate.innerHTML = (100 * (attempts - successes - wrongInputs)) / attempts;
   anlAverageDelta.innerHTML = deltaSum / attempts;
 }
 
 function checkTime() {
   if (timeJump <= 0 || timeThrow <= 0) return;
 
-  // https://playvalorant.com/en-us/news/game-updates/valorant-patch-notes-8-01/#:~:text=We%20have%20implemented,the%20same%20location.
   const delta = timeThrow - timeJump;
   const isSuccess = 0 <= delta && delta <= 100;
   insertResult(isSuccess, false, delta);
@@ -68,6 +70,10 @@ function onThrowInput() {
   if (timeThrow > 0) {
     insertResult(false, true, 0);
     timeThrow = 0;
+    if (++aceCounter > 4) {
+      document.getElementById("ace").play();
+      aceCounter = 0;
+    }
     return;
   }
   timeThrow = performance.now();
@@ -81,12 +87,12 @@ function setInputListeners() {
     event.preventDefault();
     if (event.type === jumpAssign.type && event.code === jumpAssign.code) onJumpInput();
     if (event.type === throwAssign.type && event.code === throwAssign.code) onThrowInput();
-  }
+  };
   document.onmousedown = (event) => {
     event.preventDefault();
     if (event.type === jumpAssign.type && event.button === jumpAssign.button) onJumpInput();
     if (event.type === throwAssign.type && event.button === throwAssign.button) onThrowInput();
-  }
+  };
 }
 
 setInputListeners();
@@ -98,6 +104,7 @@ document.getElementById("reset").onclick = () => {
   successes = 0;
   wrongInputs = 0;
   deltaSum = 0;
+  aceCounter = 0;
   updateAnalytics();
 
   const results = document.getElementById("results");
@@ -106,41 +113,50 @@ document.getElementById("reset").onclick = () => {
 
 document.getElementById("key-config").onclick = () => {
   const cnfDialog = document.getElementById("cnf-dialog");
-  const cnfJump = document.getElementById("cnf-jump");
-  const cnfThrow = document.getElementById("cnf-throw");
+  const cnfCurJump = document.getElementById("cnf-cur-jump");
+  const cnfCurThrow = document.getElementById("cnf-cur-throw");
+  const cnfNewJump = document.getElementById("cnf-new-jump");
+  const cnfNewThrow = document.getElementById("cnf-new-throw");
   const cnfSave = document.getElementById("cnf-save");
 
-  const inputWaiter = () => new Promise((resolve) => {
-    document.onkeydown = (event) => event.preventDefault();
-    document.onmousedown = (event) => event.preventDefault();
-    document.onkeyup = (event) => {
-      event.preventDefault();
-      resolve({
-        type: "keydown",
-        code: event.code,
-        name: event.code,
-      });
-    };
-    document.onmouseup = (event) => {
-      event.preventDefault();
-      resolve({
-        type: "mousedown",
-        button: event.button,
-        name: "Mousebutton " + event.button,
-      });
-    };
-  });
+  const inputWaiter = () =>
+    new Promise((resolve) => {
+      document.onkeydown = (event) => event.preventDefault();
+      document.onmousedown = (event) => event.preventDefault();
+      document.onkeyup = (event) => {
+        event.preventDefault();
+        resolve({
+          type: "keydown",
+          code: event.code,
+          name: event.code,
+        });
+      };
+      document.onmouseup = (event) => {
+        event.preventDefault();
+        resolve({
+          type: "mousedown",
+          button: event.button,
+          name: "Mousebutton " + event.button,
+        });
+      };
+    });
 
   (async () => {
     cnfDialog.showModal();
-    cnfJump.innerHTML = "Press any key or mouse button";
-    cnfThrow.innerHTML = "";
+    cnfCurJump.innerHTML = jumpAssign.name;
+    cnfCurThrow.innerHTML = throwAssign.name;
+    cnfNewJump.innerHTML = "";
+    cnfNewThrow.innerHTML = "";
+    cnfNewJump.classList.add("inputting");
+    cnfNewThrow.classList.remove("inputting");
     cnfSave.disabled = true;
     const newJumpAssign = await inputWaiter();
-    cnfJump.innerHTML = newJumpAssign.name;
-    cnfThrow.innerHTML = "Press any key or mouse button";
+    cnfNewJump.innerHTML = newJumpAssign.name;
+    cnfNewJump.classList.remove("inputting");
+    cnfNewThrow.classList.add("inputting");
     const newThrowAssign = await inputWaiter();
-    cnfThrow.innerHTML = newThrowAssign.name;
+    cnfNewThrow.innerHTML = newThrowAssign.name;
+    cnfNewThrow.classList.remove("inputting");
 
     cnfSave.disabled = false;
     cnfSave.onclick = () => {
